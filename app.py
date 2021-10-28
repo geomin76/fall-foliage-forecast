1603f0f2fc2a289090d3838e6e3dc5a4cc5a043c
geomin76 <geom@vt.edu>
2021-10-28 14:23:36 -0400

    range

--- app.py
+++ app.py
@@ -2,33 +2,67 @@ import numpy as np
 import pandas as pd
 import cv2
 
-# Reading in the image file
-image = cv2.imread("test.jpeg")
+# Data we want to display from image: status, estimated time of peak, time stamp
 
-# https://www.pyimagesearch.com/2014/08/04/opencv-python-color-detection/
+# find the range of colors for fall
 
-# index=["color", "color_name", "hex", "R", "G", "B"]
-# csv = pd.read_csv('colors.csv', names=index, header=None)
 
-# define the list of boundaries
-boundaries = [
-	([17, 15, 100], [50, 56, 200]),
-	([86, 31, 4], [220, 88, 50]),
-	([25, 146, 190], [62, 174, 250]),
-	([103, 86, 65], [145, 133, 128])
-]
+# Read image
+img = cv2.imread("./test1.jpg")
 
-# loop over the boundaries
+# Here, you define your target color as
+# a tuple of three values: RGB
+green = [130, 158, 0]
+
+# You define an interval that covers the values
+# in the tuple and are below and above them by 20
+diff = 20
+
+# Be aware that opencv loads image in BGR format,
+# that's why the color values have been adjusted here:
+boundaries = [([green[2], green[1]-diff, green[0]-diff],
+           [green[2]+diff, green[1]+diff, green[0]+diff])]
+
+# Scale your BIG image into a small one:
+scalePercent = 0.3
+
+# Calculate the new dimensions
+width = int(img.shape[1] * scalePercent)
+height = int(img.shape[0] * scalePercent)
+newSize = (width, height)
+
+# Resize the image:
+# img = cv2.resize(img, newSize, None, None, None, cv2.INTER_AREA)
+
+
+# for each range in your boundary list:
 for (lower, upper) in boundaries:
-    # create NumPy arrays from the boundaries
-    lower = np.array(lower, dtype = "uint8")
-    upper = np.array(upper, dtype = "uint8")
-    # find the colors within the specified boundaries and apply
-    # the mask
-    mask = cv2.inRange(image, lower, upper)
-    output = cv2.bitwise_and(image, image, mask = mask)
 
-    print(output)
-	# show the images
-	# cv2.imshow("images", np.hstack([image, output]))
-	# cv2.waitKey(0)
+    # You get the lower and upper part of the interval:
+    lower = np.array(lower, dtype=np.uint8)
+    upper = np.array(upper, dtype=np.uint8)
+
+    # cv2.inRange is used to binarize (i.e., render in white/black) an image
+    # All the pixels that fall inside your interval [lower, uipper] will be white
+    # All the pixels that do not fall inside this interval will
+    # be rendered in black, for all three channels:
+    mask = cv2.inRange(img, lower, upper)
+
+    # Now, you AND the mask and the input image
+    # All the pixels that are white in the mask will
+    # survive the AND operation, all the black pixels
+    # will remain black
+    output = cv2.bitwise_and(img, img, mask=mask)
+
+    # You can use the mask to count the number of white pixels.
+    # Remember that the white pixels in the mask are those that
+    # fall in your defined range, that is, every white pixel corresponds
+    # to a green pixel. Divide by the image size and you got the
+    # percentage of green pixels in the original image:
+    ratio_green = cv2.countNonZero(mask)/(img.size/3)
+
+    # This is the color percent calculation, considering the resize I did earlier.
+    colorPercent = (ratio_green * 100) / scalePercent
+
+    # Print the color percent, use 2 figures past the decimal point
+    print('green pixel percentage:', np.round(colorPercent, 2))
