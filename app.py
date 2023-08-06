d800c7bbb9c95c833abccdd72305ee056ca59caa
geomin76 <geom@vt.edu>
2023-08-06 18:10:30 -0500

    Organization and notes

--- app.py
+++ app.py
@@ -1,1 +1,11 @@
-print("Hello World")
+from color_analysis import color_analysis
+
+# cron job to pull data from SkyWatch
+
+# store image in s3 and get URL
+url = "images/fall.jpg"
+
+# run color_analysis on image
+percentage = color_analysis(url)
+
+# store data in dynamodb
