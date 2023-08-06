850d4a9bf3892fca672600c135e929babea168f5
Geo Min <31418725+geomin76@users.noreply.github.com>
2023-08-06 19:11:00 -0400

    Initial model and notes (#7)
    * Adding initial data model
    
    * Organization and notes

--- color_analysis.py
+++ color_analysis.py
@@ -1,20 +1,27 @@
 import cv2
 import numpy as np
 
-image = cv2.imread("images/fall.jpg")
-# lower and upper range of color, in BGR! (reverse of RGB)
-lower_range = [6, 33, 95] # dark red
-upper_range = [95, 255, 255] # bright yellow
+def color_analysis(url):
+    image = cv2.imread(url)
+    # lower and upper range of color, in BGR! (reverse of RGB)
+    lower_range = [6, 33, 95] # dark red
+    upper_range = [95, 255, 255] # bright yellow
 
-# create NumPy arrays from the boundaries
-lower = np.array(lower_range, dtype = "uint8")
-upper = np.array(upper_range, dtype = "uint8")
-# find the colors within the specified boundaries and apply
-# the mask
-mask = cv2.inRange(image, lower, upper)
-output = cv2.bitwise_and(image, image, mask = mask)
-pixel_count = cv2.countNonZero(mask)/(image.size/3)
-print('pixel percentage:', pixel_count * 100)
-# show the images
-cv2.imshow("images", np.hstack([image, output]))
-cv2.waitKey(0)
+    # create NumPy arrays from the boundaries
+    lower = np.array(lower_range, dtype = "uint8")
+    upper = np.array(upper_range, dtype = "uint8")
+    # find the colors within the specified boundaries and apply
+    # the mask
+    mask = cv2.inRange(image, lower, upper)
+
+    pixel_count = cv2.countNonZero(mask)/(image.size/3)
+    pixel_percentage = pixel_count * 100
+
+    ## For printing and showing the image locally
+    # output = cv2.bitwise_and(image, image, mask = mask)
+    # print('pixel percentage:', pixel_count * 100)
+    # # show the images
+    # cv2.imshow("images", np.hstack([image, output]))
+    # cv2.waitKey(0)
+
+    return pixel_percentage
