0622f54fbdb25d85e6512c93753faf55d44a5a41
geomin76 <geom@vt.edu>
2023-08-07 17:04:16 -0500

    Adding skywatch call

---
+++ skywatch_call.py
@@ -0,0 +1,14 @@
+import requests
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+headers = {
+    'Content-Type': 'application/json',
+    'Accept': 'application/json',
+    'x-api-key': os.environ['EARTHCACHE_KEY']
+}
+
+response = requests.get("https://api.skywatch.co/earthcache/pipelines/f14b43e2-3568-11ee-ac6d-82bf3e71e06a/interval_results?sort=asc", headers=headers)
+print(response.json())
