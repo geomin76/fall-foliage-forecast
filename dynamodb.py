4ebd58264a16849a24e8fbf4e2d58f63ef8f9133
Geo Min <31418725+geomin76@users.noreply.github.com>
2023-08-10 15:23:01 -0500

    DynamoDB Script (#13)
    * Adding dynamodb script
    
    * Adding correct functions

--- dynamodb.py
+++ dynamodb.py
@@ -1,7 +1,8 @@
 import boto3
 import os
 from dotenv import load_dotenv
-
+from model import DataEntry
+from boto3.dynamodb.conditions import Key
 load_dotenv()
 
 dynamodb = boto3.client(
@@ -10,4 +11,28 @@ dynamodb = boto3.client(
     aws_secret_access_key = os.environ.get('AWS_SECRET_ACCESS_KEY'),
 )
 
-dynamodb.put_item(TableName='fall-foliage', Item={'location_id': {'S': 'hello'}})
+def insert_data(item: DataEntry) -> bool:
+    dynamodb.put_item(TableName='fall-foliage', Item={
+        'location_id': {'S': item.location_id}, 
+        'blob_url': {'S': item.blob_url},
+        'color_percentage': {'N': str(item.color_percentage)},
+        'timestamp': {'S': str(item.timestamp)}
+    })
+    return True
+
+def get_data(key: str):
+    table = boto3.resource('dynamodb').Table('fall-foliage')
+    response = table.query(
+        KeyConditionExpression=Key('location_id').eq(key)
+    )
+    items: [DataEntry] = []
+    for item in response['Items']:
+        temp_item = DataEntry(
+            blob_url = item['blob_url'], 
+            location_id = item['location_id'],
+            timestamp = item['timestamp'],
+            color_percentage = float(item['color_percentage'])
+        )
+        items.append(temp_item)
+
+    return items
