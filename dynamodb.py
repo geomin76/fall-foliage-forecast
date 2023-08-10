27ea4b201bef483228cfee99fa602c023d90ed54
geomin76 <geom@vt.edu>
2023-08-10 14:45:30 -0500

    Adding dynamodb script

--- dynamodb.py
+++ dynamodb.py
@@ -1,6 +1,7 @@
 import boto3
 import os
 from dotenv import load_dotenv
+from model import DataEntry
 
 load_dotenv()
 
@@ -10,4 +11,16 @@ dynamodb = boto3.client(
     aws_secret_access_key = os.environ.get('AWS_SECRET_ACCESS_KEY'),
 )
 
-dynamodb.put_item(TableName='fall-foliage', Item={'location_id': {'S': 'hello'}})
+def insert_data(item: DataEntry) -> bool:
+    dynamodb.put_item(TableName='fall-foliage', Item={
+        'location_id': {'S': item.location_id}, 
+        'blob_url': {'S': item.blob_url},
+        'color_percentage': {'N': str(item.color_percentage)},
+        'timestamp': {'S': str(item.timestamp)},
+        'id': {'S': str(item.id)}
+    })
+    return True
+
+def get_data():
+    response = dynamodb.scan(TableName='fall-foliage')
+    return response['Items']
