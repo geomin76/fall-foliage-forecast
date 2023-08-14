352131b3532075bafdc6bc2bd4dafc51f735ef8d
Geo Min <31418725+geomin76@users.noreply.github.com>
2023-08-14 14:58:50 -0500

    Duplicate check (#15)
    * Adding dup check
    
    * Adding comments

--- app.py
+++ app.py
@@ -3,18 +3,24 @@ from skywatch_call import get_pipelines, get_most_recent_result_from_pipeline
 import requests
 from model import SkyWatchData, DataEntry
 import os
-from dynamodb import insert_data
+from dynamodb import insert_data, get_data
 
 def main():
     # cron job to pull data from SkyWatch
     pipeline_ids = get_pipelines()
     for pipeline_id in pipeline_ids:
+        # get most recent data from SkyWatch for a pipeline
         result: SkyWatchData = get_most_recent_result_from_pipeline(pipeline_id)
         if not result:
             continue
 
-        # TODO: if timestamp already exists for this location_id in dynamo, do not add
+        # if timestamp for a certain pipeline_id already exists, no need for duplicates and ignore
+        dynamodb_data = get_data(pipeline_id)
+        if len(dynamodb_data) > 0 and dynamodb_data[-1].timestamp == result.capture_time:
+            print("Data for pipeline {} already exists for timestamp: {}".format(pipeline_id, result.capture_time))
+            continue
 
+        # retrieving image from s3 and saving locally
         img_data = requests.get(result.visual_url).content
         with open('temp.png', 'wb') as handler:
             handler.write(img_data)
@@ -22,6 +28,7 @@ def main():
         # run color_analysis on image
         percentage = color_analysis("./temp.png")
 
+        # remove image, and if image doesn't exist, pass
         try: os.remove("./temp.png")
         except OSError: pass
 
